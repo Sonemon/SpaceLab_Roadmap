6dacbfe15fefc3ccd5371c9b39dcb25f0f526c7f
Sonemon <Sonemongoo@gmail.com>
2023-09-06 07:23:18 +0300

    New changes

--- Basic_tasks/Task_4.py
+++ Basic_tasks/Task_4.py
@@ -1,15 +1,13 @@
-from turtle import title
 import requests
 from bs4 import BeautifulSoup
 
 url = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page="
 result = []
-for page_number in range(1, 20):
+for page_number in range(1, 21):
     response = requests.get(url+str(page_number))
     
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
-        
         newLaptop = []
         newDesk = []
         laptop_names = soup.find_all("a", class_="title")
